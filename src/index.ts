005fb0bd29fd647bc6ebe16dd5b2c6d57f51b397
aprincep <agustin@raytecno.es>
2024-08-06 17:47:50 +0200

    FC

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from 'astro';
+import { getCollection, getEntry } from 'astro:content';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get('slug');
+
+//http://localhost:4321/api/posts?slug=first-post
+
+  console.log('slug', slug);
+  console.log('params', params);
+  console.log('request', request);
+  console.log('url', url);  
+
+  
+  if (slug) {
+    const post = await getEntry('blog', slug);
+
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const posts = await getCollection('blog');
+
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
